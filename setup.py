e92db7aaffd3387d4f5699e5f1e22a446ee221ba
Sylvain Viollon <thefunny@gmail.com>
2009-09-27 17:57:15 +0200

    Egg skeleton

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='zeam.form',
+      version=version,
+      description="Grok based form framework",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='grok form framework',
+      author='Sylvain Viollon',
+      author_email='thefunny@gmail.com',
+      url='',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['zeam', 'zeam.form'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
