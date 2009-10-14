e9c6817e5c5504e9708c4a5183a82d91c4eb7505
Sylvain Viollon <thefunny@gmail.com>
2009-10-14 10:13:38 +0200

    FormCanvas don't need a template in fact.

--- src/zeam/form/base/form_templates.py
+++ src/zeam/form/base/form_templates.py
@@ -6,10 +6,5 @@ from zeam.form.base import form
 
 grok.templatedir('default_templates')
 
-
-class FormCanvasTemplate(pt.PageTemplate):
-    pt.view(form.FormCanvas)
-
-
 class FormTemplate(pt.PageTemplate):
     pt.view(form.Form)
